b4f0570cdeaa6dc0b19e83fa67d8bb251ab9faeb
Ubuntu <ubuntu@ip-172-31-22-110.us-west-2.compute.internal>
2026-05-04 10:44:16 +0000

    Update batch size to 2 for all OEM experiments and add requirements

--- configs/segrefiner/exp3_bnd.py
+++ configs/segrefiner/exp3_bnd.py
@@ -1,13 +1,16 @@
 """Exp 3: Chỉ Boundary-level noise"""
 _base_ = ['./segrefiner_oem_base.py']
 model = dict(diffusion_cfg=dict(noise_components=dict(use_obj=False, use_bnd=True, use_unc=False)))
-data = dict(train=dict(pipeline=[
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=False, with_label=False, with_mask=False, with_seg=True),
-    dict(type='LoadOEMCoarseMasks', use_obj=False, use_bnd=True, use_unc=False, test_mode=False),
-    dict(type='Resize', img_scale=(256, 256), keep_ratio=False),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_masks', 'coarse_masks', 'unc_map', 'edge_map']),
-]))
+data = dict(
+    train=dict(pipeline=[
+        dict(type='LoadImageFromFile'),
+        dict(type='LoadAnnotations', with_bbox=False, with_label=False, with_mask=False, with_seg=True),
+        dict(type='LoadOEMCoarseMasks', use_obj=False, use_bnd=True, use_unc=False, test_mode=False),
+        dict(type='Resize', img_scale=(256, 256), keep_ratio=False),
+        dict(type='RandomFlip', flip_ratio=0.5),
+        dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
+        dict(type='DefaultFormatBundle'),
+        dict(type='Collect', keys=['img', 'gt_masks', 'coarse_masks', 'unc_map', 'edge_map']),
+    ]),
+    train_dataloader=dict(samples_per_gpu=2)
+)
--- configs/segrefiner/exp8_all.py
+++ configs/segrefiner/exp8_all.py
@@ -49,18 +49,21 @@ model = dict(
     )
 )
 
-data = dict(train=dict(pipeline=[
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=False, with_label=False, with_mask=False, with_seg=True),
-    dict(type='LoadOEMCoarseMasks', use_obj=True, use_bnd=True, use_unc=True, test_mode=False),
-    # Lưu global view (1024→256) TRƯỚC khi crop
-    dict(type='AddGlobalView', size=256),
-    # Crop đồng bộ img + masks + unc_map + edge_map cùng vùng ngẫu nhiên 256×256
-    dict(type='RandomCropAll', crop_size=256),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_masks', 'coarse_masks', 'unc_map', 'edge_map',
-                               'global_img', 'global_gt_np', 'global_coarse_np',
-                               'global_unc_np', 'global_edge_np']),
-]))
+data = dict(
+    train=dict(pipeline=[
+        dict(type='LoadImageFromFile'),
+        dict(type='LoadAnnotations', with_bbox=False, with_label=False, with_mask=False, with_seg=True),
+        dict(type='LoadOEMCoarseMasks', use_obj=True, use_bnd=True, use_unc=True, test_mode=False),
+        # Lưu global view (1024→256) TRƯỚC khi crop
+        dict(type='AddGlobalView', size=256),
+        # Crop đồng bộ img + masks + unc_map + edge_map cùng vùng ngẫu nhiên 256×256
+        dict(type='RandomCropAll', crop_size=256),
+        dict(type='RandomFlip', flip_ratio=0.5),
+        dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
+        dict(type='DefaultFormatBundle'),
+        dict(type='Collect', keys=['img', 'gt_masks', 'coarse_masks', 'unc_map', 'edge_map',
+                                   'global_img', 'global_gt_np', 'global_coarse_np',
+                                   'global_unc_np', 'global_edge_np']),
+    ]),
+    train_dataloader=dict(samples_per_gpu=2)
+)
